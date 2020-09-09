87cb21a60ea13586281984e6c0f22b52eefc2aee
tasola <petter.tk@hotmail.com>
2020-09-09 16:18:54 +0200

    Add socket.io support, and successfully connect to the socket from the client

--- client/src/components/ChatRoom/ChatRoom.jsx
+++ client/src/components/ChatRoom/ChatRoom.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import io from 'socket.io-client'
+import { apiUrl } from '../../utils/constants'
+
+let socket
 
 const ChatRoom = () => {
-    return <div>Hi!</div>
+  useEffect(() => {
+    socket = io(apiUrl)
+    console.log(socket)
+  }, [apiUrl])
+
+  return <div>Hi!</div>
 }
 
 export default ChatRoom
--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
-const port = process.env.port || 8000
+const http = require('http')
+const socketio = require('socket.io')
+const router = require('./controllers/endpoints')
 
+const port = process.env.port || 8000
 const app = express()
 
-app.listen(port, () => console.log(`Server is running on ${port}`))
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+  console.log('New connection')
+  socket.on('disconnect', () => {
+    console.log('Lost a connection')
+  })
+})
+
+app.use('/', router)
+
+server.listen(port, () => console.log(`Server is running on ${port}`))
